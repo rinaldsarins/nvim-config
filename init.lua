d05a499bb685c0d3f1d6157fb1e289b4b1c9024d
Rinalds Arins <rinalds.arins@gmail.com>
2024-10-14 19:24:33 +0300

    starting to work on init.lua, leader remap, lines, tabs

---
+++ init.lua
@@ -0,0 +1,28 @@
+-- leader
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+
+-- clipboard
+vim.schedule(function() 
+  vim.opt.clipboard = 'unnamedplus'
+end)
+
+-- line numbers
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+-- line wrap
+vim.opt.wrap = false
+
+-- tabs
+vim.opt.tabstop = 2 -- Number of visual spaces per tab
+vim.opt.shiftwidth = 2 -- Number of spaces to use for each step of (auto)indent
+vim.opt.expandtab = true
+
+-- Case-insensitive searching UNLESS \C or one or more capital letters in the search term
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
+-- Keep signcolumn on by default
+vim.opt.signcolumn = 'yes'
+
